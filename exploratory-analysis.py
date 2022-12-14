47b54ef9fc3a756a9fff16134e58dc3c7f2e3d3d
Pedro Freire <phfreire@id.uff.br>
2022-12-14 13:42:23 -0300

    feat: snippet that plots price data

--- exploratory-analysis.py
+++ exploratory-analysis.py
@@ -1,28 +1,32 @@
-# Import the necessary libraries
 import pandas as pd
 import matplotlib.pyplot as plt
-import my_functions
 
-# Read the CSV file with portfolio data into a Pandas DataFrame
-df = pd.read_csv('data.csv')
+# Import the get_adj_close() function from the my_functions.py file
+from my_functions import get_adj_close
 
-# Read the CSV files with stock data
-adj_close = my_functions.get_adj_close(df)
+# Read in the DataFrame from the saved data
+df = pd.read_csv("data.csv")
 
-print(adj_close)
+# Call the get_adj_close() function and pass in the DataFrame
+adj_close_df = get_adj_close(df)
 
-# Calculate the summary statistics for the stock prices
-print(df.describe())
+# Plot the adjusted closing prices for each stock
+adj_close_df.plot()
 
-# Visualize the stock prices over time
-df.plot(x='Date', y='Stock', kind='line')
+# Add a legend and labels to the plot
+plt.legend(adj_close_df.columns)
+plt.xlabel("Date")
+plt.ylabel("Adjusted Closing Price")
+plt.title("Adjusted Closing Prices for Stocks in the Portfolio")
+
+# Show the plot
 plt.show()
 
 # Calculate and visualize the distribution of returns
-returns = df.pct_change()
-returns.plot(x='Date', y='Stock', kind='hist', bins=50)
-plt.show()
+#returns = df.pct_change()
+#returns.plot(x='Date', y='Stock', kind='hist', bins=50)
+#plt.show()
 
 # Calculate and visualize the correlation between the stock prices
-corr = df.corr()
-print(corr)
+#corr = df.corr()
+#print(corr)
