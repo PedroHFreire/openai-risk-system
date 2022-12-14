05f204eff62b5d3b6a80cfb3ae9675b81bbdeecd
Pedro Freire <phfreire@id.uff.br>
2022-12-14 13:25:10 -0300

    fix: Update get_adj_close() to save adj. close prices in a dataframe and return it

--- my_functions.py
+++ my_functions.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 # Define the function
 def get_adj_close(df):
+  # Create an empty dataframe
+  adj_close_df = pd.DataFrame()
+
   # Loop over the stocks in the DataFrame
   for stock in df['Stock']:
     # Read the csv file for the current stock
@@ -10,6 +13,8 @@ def get_adj_close(df):
     # Extract the adjusted closing prices from the dataframe
     adj_close = stock_df.loc[:, "Adj Close"]
 
-    # Print the adjusted closing prices for the current stock
-    print(f"Adjusted closing prices for {stock}:")
-    print(adj_close)
+    # Save the adjusted closing prices in the dataframe
+    adj_close_df.loc[:, stock] = adj_close
+
+  # Return the dataframe
+  return adj_close_df
