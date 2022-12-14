9d1e705b433cb3616ee7381a6d3462b9017a7262
Pedro Freire <phfreire@id.uff.br>
2022-12-14 14:01:01 -0300

    feat: remake of the script

--- exploratory-analysis.py
+++ exploratory-analysis.py
@@ -22,11 +22,37 @@ plt.title("Adjusted Closing Prices for Stocks in the Portfolio")
 # Show the plot
 plt.show()
 
-# Calculate and visualize the distribution of returns
-#returns = df.pct_change()
-#returns.plot(x='Date', y='Stock', kind='hist', bins=50)
-#plt.show()
+# Calculate the daily returns for each stock
+returns_df = adj_close_df.pct_change()
 
-# Calculate and visualize the correlation between the stock prices
-#corr = df.corr()
-#print(corr)
+# Create a 2 by 2 grid of subplots
+fig, axs = plt.subplots(2, 2)
+
+# Loop over the stocks in the DataFrame
+for i, stock in enumerate(returns_df.columns):
+  # Create a histogram of the daily returns for the current stock
+  axs[i//2][i%2].hist(returns_df[stock], bins=50, alpha=0.5)
+
+  # Add a legend and labels to the subplot
+  axs[i//2][i%2].legend([stock])
+  axs[i//2][i%2].set_xlabel("Daily Return")
+  axs[i//2][i%2].set_ylabel("Frequency")
+  axs[i//2][i%2].set_title(f"Distribution of Daily Returns for {stock}")
+
+# Show the plots
+plt.show()
+
+# Calculate the correlation between the stock prices
+corr_matrix = adj_close_df.corr()
+
+# Visualize the correlation matrix
+plt.imshow(corr_matrix, cmap="RdYlGn", interpolation="none")
+
+# Add labels and a colorbar to the plot
+plt.colorbar()
+tick_marks = list(range(len(corr_matrix.columns)))
+plt.xticks(tick_marks, corr_matrix.columns, rotation=45)
+plt.yticks(tick_marks, corr_matrix.columns)
+
+# Show the plot
+plt.show()
